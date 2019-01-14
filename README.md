4549ccf9c82ea63563b4b99ea9b691e16dc550b4
lqh <1040152089@qq.com>
2019-01-14 15:29:04 +0800

    统一输出格式

--- README.md
+++ README.md
@@ -6,8 +6,11 @@
 
     import {convertTime} from 'time-plant'
 
-    convertTime.timestamp(时间戳) -> 20181010101010
+    convertTime.timestamp(时间戳)
+             -> 20181010101010
 
+    convertTime.restoreStringDate('20181010101000', 100)
+             -> Wed Oct 10 2018 10:10:00 GMT+0800 (中国标准时间)
 
 
 方法说明
@@ -16,61 +19,62 @@
 convertTime
 
 
-    timestamp(时间戳, type = 1, 年月日连接符, 时分秒连接符)
+    splitDate(Date格式, type = 1, symbol(年月日连接符), sfSymbol(时分秒连接符))
+        symbol默认为    -
+        sfSymbol默认为  :
+
         type            返回
-            -> 1        20181010101010(年月日时分秒)
-            -> 2        2018-10-10(年月日)
-            -> 3        10:10(时分)
-            -> 4        2018-10-10 10:10(年月日时分)
-            -> 5        new Date
-        说明
-            把时间戳转换成  -> 20181010101000 格式 
-                            -> new Date 格式
+            -> 1        年月日时分秒(20181010101010)
+            -> 2        年月日(2018-10-10) 
+            -> 3        年月日时分秒(默认2018-10-10 10:10格式)
+            -> 4        年月日(20181010)
+            -> 5        月日(1010)
+            -> 6        日(10)
+            -> 7        时分秒(101010)
+            -> 8        时分(1010)
+            -> 9        时(10)
+            -> 10       分秒(1010)
+            -> 11       分(10)
+            -> 12       秒(10)
+            -> 13       时分秒(10:10:10) 
+            -> 14       时分(10:10) 
+            -> 15       年月日时分秒(默认2018-10-10 10:10:10格式)
+            -> 100      Date格式
 
 
-    dateTime(new date, type = 1, 年月日连接符, 时分秒连接符)
-        type            返回
-            -> 1        20181010101010(年月日时分秒)
-            -> 2        20181010(年月日)
-            -> 3        1010(月日)
-            -> 4        10(日)
-            -> 5        2018-10-10(年月日)
-            -> 6        2018-10-10 10:10(年月日时分)
-            -> 7        2018-10-10 10:10:10(年月日时分秒)
+    timestamp(时间戳, type = 100, symbol(年月日连接符), sfSymbol(时分秒连接符))
+        type -> 传参对应的返回值和splitDate一致
+
+
+    restoreStringDate(时间字符串（20181010101010）, type = 1)
+        type -> 传参对应的返回值和splitDate一致
     
         说明
-            new date装换成20181010101000格式
-    
+            字符串(20181010101000)还原成想要的时间格式
 
-    restoreJointTime(时间段(月日/日), type = 1)
+
+    restoreJointTime(时间段(月日/日), type = 100)
         type            返回
-            -> 1        new Date 格式
-            -> 2        new Date 格式
+            -> 1        Date格式
+            -> 2        Date格式
     
         说明
             还原特殊的时间段成new data,以当前时间为拼接
-            0806 (对应type = 1) -> 20180806101000 -> new Date 
-            06   (对应type = 2) -> 20181006101000 -> new Date
+            0806 (对应type = 1)  -> Date格式
+            06   (对应type = 2)  -> Date格式
 
-
-    restoreStringDate(时间字符串, type = 1)
-        type            返回
-            -> 1        new Date 格式
-    
-        说明
-            字符串(20181010101000)还原成正常new date时间格式
     
     assignTime(assignNum)
-        sonsole.log(convertTime.assignTime(0)) =>   当天的new data格式
-        sonsole.log(convertTime.assignTime(1)) =>   明天的new data格式
-        sonsole.log(convertTime.assignTime(-1)) =>  昨天的new data格式
+        sonsole.log(convertTime.assignTime(0)) =>   当天的Data格式
+        sonsole.log(convertTime.assignTime(1)) =>   明天的Data格式
+        sonsole.log(convertTime.assignTime(-1)) =>  昨天的Data格式
 
         说明
             获取距离当天的指定日期
 
     monthStartTime()
         说明
-            获得本月的开始日期new data格式
+            获得本月的开始日期data格式
 
 
 转换成es5语法(babel)
--- src/utils/convert.js
+++ src/utils/convert.js
@@ -1,112 +1,41 @@
 // 时间工厂
 const convertTime = {
-    // 时间戳转换
     /* 
         把时间戳转换成 -> 20181010101000 格式 
                        -> new Date 格式
 
                   参数 -> payload 时间戳
     */
-    timestamp(payload, type = 1, symbol = '-', sfSymbol = ':') {
-        if (!payload) {
-            return payload
-        }
-        let datas = new Date(payload)
-        var N = datas.getFullYear();
-        var Y = datas.getMonth() + 1; //获取当前月份的日期
-        var R = datas.getDate();
-        var S = datas.getHours();
-        var F = datas.getMinutes();
-        var M = datas.getSeconds();
-        if (Y.toString().length == 1) {
-            Y = `0${Y}`
-        }
-        if (S.toString().length == 1) {
-            S = `0${S}`
-        }
-        if (R.toString().length == 1) {
-            R = `0${R}`
-        }
-        if (F.toString().length == 1) {
-            F = `0${F}`
-        }
-        if (M.toString().length == 1) {
-            M = `0${M}`
-        }
-        switch (Number(type)) {
-            case 1:
-                // '20181010101010'
-                return `${N}${Y}${R}${S}${F}${M}`;
-            case 2:
-                // '2018-10-10'
-                return `${N}${symbol}${Y}${symbol}${R}`;
-            case 3:
-                // 10:10
-                return `${S}${sfSymbol}${F}`;
-            case 4:
-                // '2018-10-10 10:10'
-                return `${N}${symbol}${Y}${symbol}${R} ${S}${sfSymbol}${F}`;
-            case 5:
-                // new date格式
-                return datas
+    timestamp(payload, type = 100, symbol = '-', sfSymbol = ':') {
+        if (payload) {
+            let datas = new Date(payload)
+            return convertTime.splitDate(datas, type, symbol, sfSymbol)
+        } else {
+            return convertTime.splitDate(new Date(), type, symbol, sfSymbol)
         }
     },
 
-    // new date装换成20181010101000格式
-    dateTime(payload, type = '1', symbol = '-', sfSymbol = ':') {
-        if (payload instanceof Date) {
-            var N = payload.getFullYear();
-            var Y = payload.getMonth() + 1; //获取当前月份的日期
-            var R = payload.getDate();
-            var S = payload.getHours();
-            var F = payload.getMinutes();
-            var M = payload.getSeconds();
-            if (Y.toString().length == 1) {
-                Y = `0${Y}`
-            }
-            if (S.toString().length == 1) {
-                S = `0${S}`
-            }
-            if (R.toString().length == 1) {
-                R = `0${R}`
-            }
-            if (F.toString().length == 1) {
-                F = `0${F}`
-            }
-            if (M.toString().length == 1) {
-                M = `0${M}`
-            }
-            switch (Number(type)) {
-                case 1:
-                    // 年月日时分秒'201810101010'
-                    return `${N}${Y}${R}${S}${F}${M}`;
-                case 2:
-                    // 年月日 20181010
-                    return `${N}${Y}${R}`;
-                case 3:
-                    // 月日 1010
-                    return `${Y}${R}`;
-                case 4:
-                    // 日 10
-                    return `${R}`;
-                case 5:
-                    // 年月日
-                    return `${N}${symbol}${Y}${symbol}${R}`;
-                case 6:
-                    // 年月日时分
-                    return `${N}${symbol}${Y}${symbol}${R} ${S}${sfSymbol}${F}`;
-                case 7:
-                    // 年月日时分秒
-                    return `${N}${symbol}${Y}${symbol}${R} ${S}${sfSymbol}${F}${sfSymbol}${M}`;
-            }
+    // 字符串(20181010101000)还原成正常new date时间格式
+    restoreStringDate(payload, type = 100, symbol = '-', sfSymbol = ':') {
+        if (payload) {
+            var year = payload.substring(0, 4)
+            var month = payload.substring(4, 6)
+            var data = payload.substring(6, 8)
+            var hour = payload.substring(8, 10)
+            var min = payload.substring(10, 12)
+            var sec = payload.substring(12, 14)
+            var date = new Date()
+            date.setFullYear(year, Number(month) - 1, data)
+            date.setHours(hour, min, sec)
+            return convertTime.splitDate(date, type, symbol, sfSymbol)
+        } else {
+            return convertTime.splitDate(new Date(), type, symbol, sfSymbol)
         }
-        return payload
     },
-
     /*
         还原特殊的时间段成new data,以当前时间为拼接
         0806 (对应type = 1) -> 20181010101000 -> new Date 
-        06   (对应type = 2)   -> 20181010101000 -> new Date
+        06   (对应type = 2) -> 20181010101000 -> new Date
     */
     restoreJointTime(jointTime, type = 1) {
         let currentTiem = convertTime.dateTime(new Date())
@@ -120,29 +49,10 @@ const convertTime = {
         }
     },
 
-    // 字符串(20181010101000)还原成正常new date时间格式
-    restoreStringDate(payload, type = 1) {
-        if (payload) {
-            var year = payload.substring(0, 4)
-            var month = payload.substring(4, 6)
-            var data = payload.substring(6, 8)
-            var hour = payload.substring(8, 10)
-            var min = payload.substring(10, 12)
-            var sec = payload.substring(12, 14)
-            var date = new Date()
-            date.setFullYear(year, Number(month) - 1, data)
-            date.setHours(hour, min, sec)
-            switch (Number(type)) {
-                case 1: // 年月
-                    return date
-            }
-        }
-        return payload
-    },
     // 获取距离当天的指定日期
-    // sonsole.log(assignTime(0)) =>   当天的new data格式
-    // sonsole.log(assignTime(1)) =>   明天的new data格式
-    // sonsole.log(assignTime(-1)) =>  昨天的new data格式
+    // sonsole.log(assignTime(0)) =>   当天的Data格式
+    // sonsole.log(assignTime(1)) =>   明天的Ddata格式
+    // sonsole.log(assignTime(-1)) =>  昨天的Ddata格式
     assignTime (assignNum) {
         var today = new Date();
         // getDate 可返回月份的某一天。
@@ -156,6 +66,87 @@ const convertTime = {
         let nowYear = now.getYear()
         nowYear += (nowYear < 2000) ? 1900 : 0
         return new Date(nowYear, nowMonth, 1)
-    }
+    },
+    // 组合拆分new data格式
+    splitDate (payload, type = 1, symbol = '-', sfSymbol = ':') {
+        let datas
+        if (payload instanceof Date) {
+            datas = payload
+        } else {
+            datas = new Date()
+        }
+        var N = datas.getFullYear();
+        var Y = datas.getMonth() + 1; //获取当前月份的日期
+        var R = datas.getDate();
+        var S = datas.getHours();
+        var F = datas.getMinutes();
+        var M = datas.getSeconds();
+        if (Y.toString().length === 1) {
+            Y = `0${Y}`
+        }
+        if (R.toString().length === 1) {
+            R = `0${R}`
+        }
+        if (S.toString().length === 1) {
+            S = `0${S}`
+        }
+        if (F.toString().length === 1) {
+            F = `0${F}`
+        }
+        if (M.toString().length === 1) {
+            M = `0${M}`
+        }
+
+        switch (Number(type)) {
+            case 1:
+                // 年月日时分秒 201810101010
+                return `${N}${Y}${R}${S}${F}${M}`
+            case 2:
+                // 年月日(默认2018-10-10格式)
+                return `${N}${symbol}${Y}${symbol}${R}`
+            case 3:
+                // 年月日时分(默认2018-10-10 10:10格式)
+                return `${N}${symbol}${Y}${symbol}${R} ${S}${sfSymbol}${F}`
+            case 4:
+                // 年月日 20181010
+                return `${N}${Y}${R}`
+            case 5:
+                // 月日 1010
+                return `${Y}${R}`
+            case 6:
+                // 日 10
+                return `${R}`
+            case 7:
+                // 时分秒(101010)
+                return `${S}${F}${M}`
+            case 8:
+                // 时分(1010)
+                return `${S}${F}`
+            case 9:
+                // 时(10)
+                return `${S}`
+            case 10:
+                // 分秒(1010)
+                return `${F}${M}`
+            case 11:
+                // 分(10)
+                return `${F}`
+            case 12:
+                // 秒(10)
+                return `${M}`
+            case 13:
+                // 时分秒(默认10:10:10格式)
+                return `${S}${sfSymbol}${F}${sfSymbol}${M}`
+            case 14:
+                // 时分秒(默认10:10格式)
+                return `${S}${sfSymbol}${F}`
+            case 15:
+                // 年月日时分秒(默认2018-10-10 10:10:10格式)
+                return `${N}${symbol}${Y}${symbol}${R} ${S}${sfSymbol}${F}${sfSymbol}${M}`
+            case 100:
+                // new date格式
+                return datas
+        }
+    },
 }
 export default convertTime;
